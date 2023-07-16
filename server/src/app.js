80b9ee0cf126b87b9191c3089a8d739605e63f36
DylanSuarez <2000sajo@gmail.com>
2023-07-15 19:16:51 -0500

    Rout Not Found

--- server/src/app.js
+++ server/src/app.js
@@ -18,6 +18,12 @@ export async function startApolloServer(typeDefs, resolvers) {
 
   app.use("/graphql", cors(), express.json(), expressMiddleware(server))
 
+  app.use((req, res) => {
+    res.status(404).json({
+      error: 'Rout Not Found'
+    })
+  })
+
   await new Promise((resolve) => httpServer.listen({ port: env.PORT }, resolve))
   console.log(`🚀 Server ready at http://localhost:${env.PORT}`)
 }
