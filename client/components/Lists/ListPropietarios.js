742063d47f2eb84ef6dc85e396277934399f6447
DylanSuarez <2000sajo@gmail.com>
2023-08-11 10:29:45 -0500

    listpropietarios

--- client/components/Lists/ListPropietarios.js
+++ client/components/Lists/ListPropietarios.js
@@ -1,42 +1,49 @@
+import { List } from './List'
+import ModalForm from '../ModalForm'
+
 import { Button } from 'antd'
+import { useState } from 'react'
 
 export default function ListPropietarios ({ data }) {
-  return (
-    <div style={{ border: '1px solid #fff' }}>
-      {
-        data.property.propietario <= 0
-          ? (
-            <div>
-              <h2>No tiene Propietarios</h2>
-              <button>Registrar propietario</button>
-            </div>
-            )
+  const [toggle, setToggle] = useState(false)
 
-          : (
-            <div>
-              <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-                <h3>Propietarios registrados</h3>
-                <Button type='primary'>Crear Propietario</Button>
-              </div>
-              {
-                data.property.propietario.map((owner, i) => (
-                  <ul key={owner.id} style={{ border: '1px solid #fff' }}>
-                    <li>{owner.nombres}</li>
-                    <li>{owner.apellidos}</li>
-                    <li>{owner.tipo_documento}</li>
-                    <li>{owner.num_celular}</li>
-                    <li>{owner.razon_social}</li>
-                    <li>{owner.direccion}</li>
-                    <li>{owner.tipo_documento}</li>
-                    <li>{owner.tipo_propietario}</li>
-                    <Button type='primary'>Editar</Button>
-                    <Button danger>Eliminar</Button>
-                  </ul>
-                ))
-              }
-            </div>
-            )
+  return (
+    <>
+      <List data={data.property.propietario} btnMessaje='Registrar propietario' errorTitle='No hay propietarios' setToggle={setToggle}>
+        <Propietario data={data} />
+      </List>
+
+      <ModalForm title='Crear Construccion' toggle={toggle} setToggle={setToggle}>
+        <h3>Formulario propietario</h3>
+      </ModalForm>
+    </>
+  )
+}
+
+export function Propietario ({ data }) {
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <h3>Propietarios registrados</h3>
+        <Button type='primary'>Crear Propietario</Button>
+      </div>
+      {
+        data.property.propietario.map((owner) => (
+          <ul key={owner.id} style={{ border: '1px solid #fff' }}>
+            <li>{owner.nombres}</li>
+            <li>{owner.apellidos}</li>
+            <li>{owner.tipo_documento}</li>
+            <li>{owner.num_celular}</li>
+            <li>{owner.razon_social}</li>
+            <li>{owner.direccion}</li>
+            <li>{owner.tipo_documento}</li>
+            <li>{owner.tipo_propietario}</li>
+            <Button type='primary'>Editar</Button>
+            <Button danger>Eliminar</Button>
+          </ul>
+        ))
       }
-    </div>
+
+    </>
   )
 }
