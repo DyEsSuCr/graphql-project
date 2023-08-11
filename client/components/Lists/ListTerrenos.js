4d2c5f2ed12302ff3ae11a6e1cabd1abcb11664f
DylanSuarez <2000sajo@gmail.com>
2023-08-10 20:51:02 -0500

    List

--- client/components/Lists/ListTerrenos.js
+++ client/components/Lists/ListTerrenos.js
@@ -6,6 +6,7 @@ import { GET_PREDIO_TERRENO } from '../../graphql/predios'
 import { Button } from 'antd'
 import { useState } from 'react'
 import { useMutation, useQuery } from '@apollo/client'
+import { List } from './List'
 
 export default function ListTerrenos ({ data, predioId }) {
   const [toggle, setToggle] = useState(false)
@@ -29,32 +30,21 @@ export default function ListTerrenos ({ data, predioId }) {
   })
 
   return (
-    <div style={{ border: '1px solid #fff' }}>
-      {
-        !data.property.terreno
-          ? (
-            <div>
-              <h2>No tiene terreno</h2>
-              <Button type='primary' onClick={() => setToggle(true)}>Registrar terreno</Button>
-            </div>
-            )
-
-          : (
-            <div>
-              <h2>Terreno registrado</h2>
-              <p>precio comercial: {data.property.terreno.precio_comercial}</p>
-              <p>area: {data.property.terreno.area}</p>
-              <p>tipo_terreno: {data.property.terreno.tipo_terreno}</p>
-              <p>cerca_fuentes: {data.property.terreno.cerca_fuentes ? 'si' : 'no'}</p>
-              <Button type='primary' onClick={() => setToggle(true)}>Editar</Button>
-              <Button danger onClick={() => removeTerreno({ variables: { id: data.property.terreno.id } })}>Eliminar</Button>
-            </div>
-            )
-      }
+    <>
+      <List btnMessaje='Registrar terreno' data={data.property.terreno} setToggle={setToggle} errorTitle='No tiene terreno'>
+        <h2>Terreno registrado</h2>
+        <p>precio comercial: {data.property.terreno?.precio_comercial}</p>
+        <p>area: {data.property.terreno?.area}</p>
+        <p>tipo_terreno: {data.property.terreno?.tipo_terreno}</p>
+        <p>cerca_fuentes: {data.property.terreno?.cerca_fuentes ? 'si' : 'no'}</p>
+        <Button type='primary' onClick={() => setToggle(true)}>Editar</Button>
+        <Button danger onClick={() => removeTerreno({ variables: { id: data.property.terreno.id } })}>Eliminar</Button>
+      </List>
 
       <ModalForm title='Crear Terreno' toggle={toggle} setToggle={setToggle} predioId={predioId}>
         <FormTerreno setToggle={setToggle} predioId={predioId} updateData={terrenoData} />
       </ModalForm>
-    </div>
+    </>
+
   )
 }
