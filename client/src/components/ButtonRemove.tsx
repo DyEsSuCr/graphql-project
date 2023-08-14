e774e7d5b98e84bcaae51b8daea2e08b1807b8a0
DylanSuarez <2000sajo@gmail.com>
2023-08-14 11:08:38 -0500

    buttonRemove...

--- client/src/components/ButtonRemove.tsx
+++ client/src/components/ButtonRemove.tsx
@@ -4,6 +4,7 @@ import { DELETE_PROPERTY } from '@/graphql/propertys/mutations'
 import { useMutation } from '@apollo/client'
 import { useRouter } from 'next/navigation'
 import { Button } from 'antd'
+import { GET_PROPERTYS } from '@/graphql/propertys/querys'
 
 interface Props {
   id: string
@@ -11,7 +12,15 @@ interface Props {
 
 export const ButtonRemove = ({ id }: Props) => {
   const router = useRouter()
-  const [deleteProperty] = useMutation(DELETE_PROPERTY)
+  const [deleteProperty] = useMutation(DELETE_PROPERTY, {
+    refetchQueries: [
+      {
+        query: GET_PROPERTYS
+      },
+      'GET_PROPERTYS'
+    ],
+    awaitRefetchQueries: true
+  })
 
   const remove = () => {
     void deleteProperty({ variables: { id } })
@@ -22,3 +31,36 @@ export const ButtonRemove = ({ id }: Props) => {
     <Button danger onClick={remove}>Eliminar</Button>
   )
 }
+
+/* 'use client'
+
+import { DELETE_PROPERTY } from '@/graphql/propertys/mutations'
+import { useMutation } from '@apollo/client'
+import { useRouter } from 'next/navigation'
+import { Button } from 'antd'
+import { DELETE_BUILD } from '@/graphql/builds/mutations'
+import { DELETE_LAND } from '@/graphql/lands/mutations'
+
+interface Props {
+  id: string
+  target: string
+}
+
+export const ButtonRemove = ({ id, target }: Props) => {
+  const router = useRouter()
+  const [deleteProperty] = useMutation(DELETE_PROPERTY)
+  const [deleteBuild] = useMutation(DELETE_BUILD)
+  const [deleteLand] = useMutation(DELETE_LAND)
+
+  const remove = {
+    land: void deleteProperty({ variables: { id } }),
+    build: void deleteBuild({ variables: { id } }),
+    property: void deleteLand({ variables: { id } })
+  }
+
+  const selectRemove = remove[target]
+
+  return (
+    <Button danger onClick={selectRemove}>Eliminar</Button>
+  )
+} */
