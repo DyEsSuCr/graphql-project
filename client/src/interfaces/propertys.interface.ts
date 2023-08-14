0c9bc47d74c6d4f469113f5efb3f74b676a8573c
DylanSuarez <2000sajo@gmail.com>
2023-08-13 20:28:02 -0500

    interfaces

--- client/src/interfaces/propertys.interface.ts
+++ client/src/interfaces/propertys.interface.ts
@@ -5,3 +5,19 @@ export interface Property {
   municipio: string
   departamento: string
 }
+
+export interface Land {
+  id?: string
+  area: number
+  cerca_fuentes: boolean
+  precio_comercial: number
+  tipo_terreno: string
+}
+
+export interface Build {
+  id?: string
+  area: number
+  pisos: number
+  direccion: string
+  tipo_construccion: string
+}
