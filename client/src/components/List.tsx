4cd4da0273671cd495d4c059b0b9964987cf867a
DylanSuarez <2000sajo@gmail.com>
2023-08-13 20:27:10 -0500

    List

---
+++ client/src/components/List.tsx
@@ -0,0 +1,21 @@
+interface Props {
+  data: Object | Object[]
+  errorTitle: string
+  children: React.ReactNode
+}
+
+export function List ({ data, errorTitle, children }: Props) {
+  if (!data) {
+    return (
+      <div className='flex justify-between items-center border border-white'>
+        <h2>{errorTitle}</h2>
+      </div>
+    )
+  }
+
+  return (
+    <div className='border border-white'>
+      {children}
+    </div>
+  )
+}
